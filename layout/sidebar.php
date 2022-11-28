6612991d77fbea8cb6195bfac49791e65eb89630
atiamohammad30 <amohammad183002@bscse.uiu.ac.bd>
2022-11-28 10:57:03 +0600

    Layout

---
+++ layout/sidebar.php
@@ -0,0 +1,52 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="add_pharmacy.php" class="nav-link active" aria-current="page">
+                Add Pharmacy
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pharmacy_list.php" class="nav-link text-white" >
+                Pharmacy List
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="add_medicine.php" class="nav-link text-white" aria-current="page">
+                Add Medicine
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="medicine_list.php" class="nav-link text-white" aria-current="page">
+                Medicine List
+            </a>
+        <li class="nav-item">
+            <a href="add_medicine_pharm.php" class="nav-link text-white" aria-current="page">
+                Add Medicine to Pharmacy
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="orders.php" class="nav-link text-white" aria-current="page">
+                Orders
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="add_pharmacy.php" class="nav-link text-white" aria-current="page">
+                Users
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="logout.php" class="nav-link text-white" aria-current="page">
+               Logout
+            </a>
+        </li>
+
+    </ul>
+</div>
+
+<div class="b-example-divider" style="width: 0 !important;"></div>
---
+++ layout/sidebar2.php
@@ -0,0 +1,71 @@
+<?php
+  session_start();
+  // var_dump($_SESSION);exit();
+  if(!isset($_SESSION['user'])){
+    header("Location: http://localhost/pharmacheck/admin/login.php");
+  }
+?>
+<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;background: #898585!important;">
+    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-5 fw-semibold">Collapsible</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          Pharmacy
+        </button>
+        <div class="collapse show" id="home-collapse" style="">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="add_pharmacy.php" class="link-dark rounded">Add Pharmacy</a></li>
+            <li><a href="pharmacy_list.php" class="link-dark rounded">Pharmacy List</a></li>
+            <li><a href="add_medicine.php" class="link-dark rounded">Add Medicine</a></li>
+            <li><a href="add_medicine_pharm.php" class="link-dark rounded">Add Medicine to Pharmacy</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          Dashboard
+        </button>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Overview</a></li>
+            <li><a href="#" class="link-dark rounded">Weekly</a></li>
+            <li><a href="#" class="link-dark rounded">Monthly</a></li>
+            <li><a href="#" class="link-dark rounded">Annually</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          Orders
+        </button>
+        <div class="collapse" id="orders-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">New</a></li>
+            <li><a href="#" class="link-dark rounded">Processed</a></li>
+            <li><a href="#" class="link-dark rounded">Shipped</a></li>
+            <li><a href="#" class="link-dark rounded">Returned</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="border-top my-3"></li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+          Account
+        </button>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">New...</a></li>
+            <li><a href="#" class="link-dark rounded">Profile</a></li>
+            <li><a href="#" class="link-dark rounded">Settings</a></li>
+            <li><a href="process/logout.php" class="link-dark rounded">Sign out</a></li>
+          </ul>
+        </div>
+      </li>
+      <li style=""><a href="logout.php" class="link-dark rounded">Logout</a></li>
+    </ul>
+  </div>
+
+  <div class="b-example-divider" style="width: 0 !important;"></div>
